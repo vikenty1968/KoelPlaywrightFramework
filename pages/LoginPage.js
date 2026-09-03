fd4f925fc78bbe81ab41b70cf823a98a46c3fab0
vikenty19 <vikenty.plakhov@testpro.io>
2026-09-03 17:23:34 -0400

    Add negative test

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -4,6 +4,8 @@ class LoginPage{
         this.email = page.locator("[type='email']");
         this.password = page.locator("[type='password']")
         this.login= page.getByRole('button',{name:'Log In'})
+        this.koelLogo = page.locator('div.logo')
+        this.loginForm = page.getByTestId('login-form')
     }
     async loginUser(email,password){
        await this.email.fill(email)
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,7 @@
 import{test,expect} from '@playwright/test';
 import { LoginPage }  from '../pages/LoginPage';      
 import { HomePage } from '../pages/HomePage';
+const invalidPassword = '123qwe'
 test("Login Success",async({page})=>{
     const loginPage = new LoginPage(page);
     const homepage = new HomePage(page)
@@ -8,4 +9,11 @@ test("Login Success",async({page})=>{
     await loginPage.loginUser(process.env.KOEL_EMAIL,process.env.KOEL_PASSWORD)
     await expect(homepage.logOut).toBeVisible();
 
-});
+})
+test("Login with invalid password",async({page})=>{
+    await page.goto('/')
+const loginPage= new LoginPage(page)
+  await loginPage.loginUser(process.env.KOEL_EMAIL,invalidPassword)
+  await expect(loginPage.koelLogo).toBeVisible();
+  await expect(loginPage.loginForm).toHaveClass(/error/)
+})
