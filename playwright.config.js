615d9222a28b482aa758960d4f4f7eae86df78db
vikenty19 <vikenty.plakhov@testpro.io>
2026-09-04 14:20:49 -0400

    Refactor login tests with fixtures amd add validation

--- playwright.config.js
+++ playwright.config.js
@@ -29,8 +29,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL:process.env.BASE_URL,
+    locale:'en-US',
+      launchOptions: {
+        args: ['--lang=en-US']
+    },
     // baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,5 +1,6 @@
 import{test,expect} from '../fixtures/fixtures.js'
 const invalidPassword = '123qwe'
+const warningMessage = "Please fill out this field."
 test("Login Success",async({loginPage,homePage})=>{
    
     await loginPage.openLoginPage()
@@ -12,4 +13,10 @@ test("Login with invalid password",async({loginPage})=>{
   await loginPage.loginUser(process.env.KOEL_EMAIL,invalidPassword)
   await expect(loginPage.koelLogo).toBeVisible();
   await expect(loginPage.loginForm).toHaveClass(/error/)
+})
+test("Login with empty password", async({loginPage})=>{
+    await loginPage.openLoginPage();
+    await loginPage.loginUser(process.env.KOEL_EMAIL,"");//empty password
+    const infoMessge = await loginPage.password.evaluate(element =>element.validationMessage)
+    expect(infoMessge).toBe(warningMessage);
 })
